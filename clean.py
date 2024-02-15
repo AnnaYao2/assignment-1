7bf9a8d124115eb062e47798660534a20dc96b4d
Anna YAO <13451752789@163.com>
2024-02-16 00:52:54 +0800

    data cleaning script

---
+++ clean.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def clean(input1, input2):
+    df1 = pd.read_csv(input1)
+    df2 = pd.read_csv(input2)
+    merged_df = pd.merge(df1, df2, left_on='respondent_id', right_on='id')
+    merged_df = merged_df.drop(columns=['id'])
+    merged_df = merged_df.dropna()
+    merged_df = merged_df[~merged_df['job'].str.contains('insurance|Insurance')]
+    return merged_df
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input1', help='respondent_contact (CSV)')
+    parser.add_argument('input2', help='respondent_other (CSV)')
+    parser.add_argument('output', help='Cleaned data file (CSV)')
+    args = parser.parse_args()
+
+    cleaned = clean(args.input1, args.input2)
+    cleaned.to_csv(args.output, index=False)
